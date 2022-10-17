16bda36a2a98b232ae1e704e123e109b364e5eda
Karen Verçosa <113350208+karenvcsa@users.noreply.github.com>
2022-10-17 11:57:53 -0300

    base e comandos basicos nave

--- jogo.py
+++ jogo.py
@@ -6,14 +6,32 @@ from sys import exit
 pygame.init()
 
 largura = 600
-altura = 820
+altura = 700
+x = 0
+
 
 tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Space Invaders')
+relogio = pygame.time.Clock()
 
 
 while True:
+    relogio.tick(40)
+    tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+        
+    if pygame.key.get_pressed()[K_LEFT]:
+        x = x - 20 
+    if pygame.key.get_pressed()[K_RIGHT]:
+        x = x + 20
+
+
+    pygame.draw.rect(tela, (0, 255, 0), (x, 600, 50, 50))
+
+    
     pygame.display.update()
+
+
