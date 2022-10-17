80c05911a5a58b6c051e4ef8d50002a7dfd4ea41
Karen Verçosa <113350208+karenvcsa@users.noreply.github.com>
2022-10-17 13:37:41 -0300

    correção class da nave

--- jogo.py
+++ jogo.py
@@ -1,37 +1,66 @@
 import pygame
+
 from pygame.locals import *
 from sys import exit
 
 
 pygame.init()
 
-largura = 600
-altura = 700
+#TELA DO JOGO
+LARGURA = 600
+ALTURA = 700
 x = 0
 
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Space Invaders")
 
-tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Space Invaders')
+
 relogio = pygame.time.Clock()
 
+nave = pygame.Surface((50, 50))
+nave.fill((255, 0, 0))
 
+tela.blit(nave, (50, 50))
+
+# CARACTERÍSTICAS DA NAVE
+class Nave():
+    def __init__(self):
+        self.x = 275
+        self.y = 600
+
+    def aparecer(self):
+        tela.blit(nave, (self.x, self.y))
+
+    def andarEsquerda(self):
+        if self.x >= 0:
+            self.x -= 20
+
+    def andarDireita(self):
+        if self.x <= 470:
+            self.x += 20
+
+
+navezinha = Nave()
+
+
+#LOOP DO JOGO
 while True:
-    relogio.tick(40)
+    relogio.tick(60)
     tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        
+
     if pygame.key.get_pressed()[K_LEFT]:
-        x = x - 20 
-    if pygame.key.get_pressed()[K_RIGHT]:
-        x = x + 20
+        navezinha.andarEsquerda()
+
+    elif pygame.key.get_pressed()[K_RIGHT]:
+        navezinha.andarDireita()
+
+    navezinha.aparecer()
 
 
-    pygame.draw.rect(tela, (0, 255, 0), (x, 600, 50, 50))
-
-    
     pygame.display.update()
 
 
