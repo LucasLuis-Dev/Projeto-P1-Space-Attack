fd0e60ff49dbdfeafe2e7b94125f49cfb624557c
Karen Verçosa <113350208+karenvcsa@users.noreply.github.com>
2022-10-17 11:27:40 -0300

    base e movimentos básicos da nave

---
+++ jogo.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+
+pygame.init()
+
+largura = 600
+altura = 820
+
+tela = pygame.display.set_mode((largura, altura))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
