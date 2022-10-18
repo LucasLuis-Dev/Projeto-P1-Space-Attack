c6afea101d90ced647e193e059dcd74e938dfec5
Mateus <mso3@cin.ufpe.br>
2022-10-18 20:20:35 -0300

    Commit inicial

---
+++ projeto.py
@@ -0,0 +1,76 @@
+import pygame
+import os
+import time
+import random
+pygame.init()
+
+
+nave_inimiga = pygame.image.load(os.path.join('nave_inimiga.png'))
+
+laser = pygame.image.load(os.path.join('laser.png'))
+
+class Laser:
+    def __init__(self,x,y,img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    
+    def draw(self, window):
+        window.blit(self.img, (self.x,self.y))
+
+    def move(self, vel):
+        self.y += vel
+    
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+
+    def collision(self, obj):
+        return collide(self, obj)
+    
+class Nave:
+    cooldown = 30
+
+    def __init__(self,x,y,vida=100):
+        self.x = x
+        self.y = y
+        self.vida = vida
+        self.nave_inimiga_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.nave_inimiga_img, (self.x,self.y))
+        for laser in self.lasers:
+            laser.draw(window) 
+    
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(altura):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.vida -= 10
+                self.lasers.remove(laser)
+    
+    def cooldown(self):
+        if self.cool_down_counter >= self.cooldown:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
+    
+    def get_width(self):
+        return self.nave_inimiga_img.get_width()
+    
+    def get_height(self):
+        return self.nave_inimiga_img.get_height()
+
+
