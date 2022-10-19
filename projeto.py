ff0522bfb9f6415ab3689e9982a1c8d315f81550
Mateus <mso3@cin.ufpe.br>
2022-10-18 21:05:58 -0300

    altura da tela

--- projeto.py
+++ projeto.py
@@ -9,29 +9,31 @@ nave_inimiga = pygame.image.load(os.path.join('nave_inimiga.png'))
 
 laser = pygame.image.load(os.path.join('laser.png'))
 
+
 class Laser:
-    def __init__(self,x,y,img):
+    def __init__(self, x, y, img):
         self.x = x
         self.y = y
         self.img = img
         self.mask = pygame.mask.from_surface(self.img)
-    
+
     def draw(self, window):
-        window.blit(self.img, (self.x,self.y))
+        window.blit(self.img, (self.x, self.y))
 
     def move(self, vel):
         self.y += vel
-    
+
     def off_screen(self, height):
         return not(self.y <= height and self.y >= 0)
 
     def collision(self, obj):
         return collide(self, obj)
-    
+
+
 class Nave:
     cooldown = 30
 
-    def __init__(self,x,y,vida=100):
+    def __init__(self, x, y, vida=100):
         self.x = x
         self.y = y
         self.vida = vida
@@ -41,20 +43,20 @@ class Nave:
         self.cool_down_counter = 0
 
     def draw(self, window):
-        window.blit(self.nave_inimiga_img, (self.x,self.y))
+        window.blit(self.nave_inimiga_img, (self.x, self.y))
         for laser in self.lasers:
-            laser.draw(window) 
-    
+            laser.draw(window)
+
     def move_lasers(self, vel, obj):
         self.cooldown()
         for laser in self.lasers:
             laser.move(vel)
-            if laser.off_screen(altura):
+            if laser.off_screen('''aqui irá colocar a altura da tela'''):
                 self.lasers.remove(laser)
             elif laser.collision(obj):
                 obj.vida -= 10
                 self.lasers.remove(laser)
-    
+
     def cooldown(self):
         if self.cool_down_counter >= self.cooldown:
             self.cool_down_counter = 0
@@ -66,11 +68,9 @@ class Nave:
             laser = Laser(self.x, self.y, self.laser_img)
             self.lasers.append(laser)
             self.cool_down_counter = 1
-    
+
     def get_width(self):
         return self.nave_inimiga_img.get_width()
-    
+
     def get_height(self):
         return self.nave_inimiga_img.get_height()
-
-
