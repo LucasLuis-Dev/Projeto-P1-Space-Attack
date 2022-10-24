8d2c38afab10bf62739124fd9f68cc48a5537db6
Karen Verçosa <113350208+karenvcsa@users.noreply.github.com>
2022-10-24 10:08:39 -0300

    adição de sprite da nave

--- jogo.py
+++ jogo.py
@@ -9,27 +9,34 @@ pygame.init()
 #TELA DO JOGO
 LARGURA = 600
 ALTURA = 700
-x = 0
-
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Space Invaders")
 
 
 relogio = pygame.time.Clock()
 
-nave = pygame.Surface((50, 50))
-nave.fill((255, 0, 0))
-
-tela.blit(nave, (50, 50))
-
 # CARACTERÍSTICAS DA NAVE
-class Nave():
+class Nave(pygame.sprite.Sprite):
     def __init__(self):
-        self.x = 275
-        self.y = 600
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('./naveimagens/1 (2).png'))
+        self.sprites.append(pygame.image.load('./naveimagens/2.png'))
+        self.sprites.append(pygame.image.load('./naveimagens/3.png'))
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (369-200, 360-200))
 
-    def aparecer(self):
-        tela.blit(nave, (self.x, self.y))
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = LARGURA/2 - 85, ALTURA - 170
+
+    def update(self):
+        self.atual = self.atual + 0.2
+        if self.atual >= len(self.sprites):
+            self.atual = 0
+        self.image = self.sprites[int(self.atual)]
+        self.image = pygame.transform.scale(self.image, (369-200, 360-200))
 
     def andarEsquerda(self):
         if self.x >= 0:
@@ -40,6 +47,9 @@ class Nave():
             self.x += 20
 
 
+todas_as_sprites = pygame.sprite.Group()   
+imgnave = Nave()
+todas_as_sprites.add(imgnave)
 navezinha = Nave()
 
 
@@ -58,8 +68,8 @@ while True:
     elif pygame.key.get_pressed()[K_RIGHT]:
         navezinha.andarDireita()
 
-    navezinha.aparecer()
-
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
 
     pygame.display.update()
 
