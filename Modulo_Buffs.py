7281f8ee450de02099687dce2d1e5eba31d27664
Lucasluis1407 <lucasluisouza@gmail.com>
2022-10-29 02:29:05 -0300

    Jogo Completo versão 1

--- Modulo_Buffs.py
+++ Modulo_Buffs.py
@@ -1,11 +1,8 @@
-from math import radians
 import pygame
-from random import randint, choice
+from random import randint
 
 
 class Buffs():
-
-
     def __init__(self):
         self.y = -50
         self.x = randint(0,500)
@@ -16,11 +13,16 @@ class Buffs():
         self.buffCoracao = pygame.image.load('./assets/CoraçãoPixel.png')
         self.buffArmamento = pygame.image.load('./assets/ArmaPixel.png')
         self.buffEstrela = pygame.image.load('./assets/EstrelaPixel.png')
-        self.larguraBuffs = self.buffCoracao.get_width()
-        self.alturaBuffs = self.buffCoracao.get_height()
+        self.largura = self.buffCoracao.get_width()
+        self.altura = self.buffCoracao.get_height()
 
+
+    # METODOS DA CLASSE
 
     def aparecerBuff(self,tela, elemento):
+        """
+            Mostra os buffs na tela, se eles não estiverem passado dos limites da tela
+        """
         if self.y < 900 and self.quedaBuff == False:
             
             if elemento == 'coracao':
@@ -43,8 +45,10 @@ class Buffs():
             self.y = -50
 
 
-
     def contador(self):
+        """
+            Cálculo o tempo para o surgimento e intervalo entre a queda de cada buff
+        """
         if self.tempoAparecimento != 400:
             self.tempoAparecimento += 1
             return False
@@ -53,18 +57,36 @@ class Buffs():
             self.quedaBuff = False
             return True
     
+
     def escolhaBuff(self):
+        """
+            Escolhe o buff que irá cair de forma aleatória
+        """
         escolha = self.nomesBuffs[randint(0,2)]
         self.buffEscolhido = escolha
         return escolha
 
     
     def verificaEscolhaBuff(self):
+        """
+            Verifica se o buff foi escolhido
+        """
         if self.buffEscolhido:
             return True
 
         else:
             return False
+
+
+    def recomecarJogo(self):
+        """
+            Reseta os atributos para recomeçar o jogo
+        """
+        self.y = -50
+        self.x = randint(0,500)
+        self.tempoAparecimento = 0
+        self.quedaBuff = False
+        self.buffEscolhido = False
         
 
 
--- jogo.py
+++ jogo.py
@@ -1,8 +1,9 @@
 import pygame
 from random import randint
-import os
-from Modulo_Jogador import Nave, jogador
+from Modulo_Jogador import Nave, jogador, DisparoNave
 from Modulo_Buffs import Buffs
+from Modulo_NaveInimiga import NaveInimiga, disparoNaveInimiga
+from Modulo_Asteroides import Asteroides
 
 from pygame.locals import *
 from sys import exit
@@ -10,46 +11,59 @@ from sys import exit
 
 pygame.init()
 
-# Constantes do tamanho da plataforma do jogo
+# CONSTANTES DO TAMANHO DA TELA DO JOGO
 
 LARGURA = 600
 ALTURA = 820
 
 
-# Variavel para controlar os frames da tela
+# VARIAVEIS DE CONTROLE DO JOGO
 
 relogio = pygame.time.Clock()
+pygame.mixer.music.set_volume(50)
+musicaFundo = pygame.mixer.music.load('./Music/MusicaFundo.mp3')
+pygame.mixer.music.play(-1)
 
 
-# Variaveis responsaveis pela estetica
+# VARIAVEIS RESPONSAVEIS PELA ESTÉTICA BÁSICA DO JOGO
 
-imagemFundo = pygame.image.load('./assets/fundo_2.png')
-imagemTelaInicial = pygame.image.load('./assets/TelaInicial.png')
-imagemTelaFinal = pygame.image.load('./assets/TelaFinal.png')
-gameIcon = pygame.image.load('./assets/icon.png')
-pygame.display.set_icon(gameIcon)
-
-fonteTextoGame = pygame.font.SysFont('arial', 30 , True, False)
+IMAGEMFUNDO = pygame.image.load('./assets/fundo_2.png')
+IMAGEMTELAINICIAL = pygame.image.load('./assets/TelaInicial.png')
+IMAGEMTELAFINAL = pygame.image.load('./assets/TelaFinal.png')
+ICONEJOGO = pygame.image.load('./assets/icon.png')
+pygame.display.set_icon(ICONEJOGO)
 
 
+# DEFINIÇÃO DA TELA E NOME DO JOGO
 
-
-tela = pygame.display.set_mode((LARGURA,ALTURA))
+TELA = pygame.display.set_mode((LARGURA,ALTURA))
 pygame.display.set_caption("Space Invaders")
 
-nave = Nave()
-player = jogador()
-buffs = Buffs()
+# CONSTANTES DAS CLASSES UTILIZADAS NO JOGO
 
+NAVE = Nave()
+PLAYER = jogador()
+BUFFS = Buffs()
+NAVEINIMIGA = NaveInimiga()
+DISPAROINIMIGO = disparoNaveInimiga()
+DISPAROPLAYER = DisparoNave()
+ASTEROIDE_1 = Asteroides()
+ASTEROIDE_2 = Asteroides()
+ASTEROIDE_3 = Asteroides()
+ASTEROIDE_4 = Asteroides()
+ASTEROIDE_5 = Asteroides()
+ 
+
+# VARIAVEL DA MAQUINA DE ESTADOS DO JOGO
 
 estadoAtual = "Tela inicial"
 
 
+# LOOPING DO JOGO
 while True:
 
     if estadoAtual == 'Tela inicial':
-        tela.blit(imagemTelaInicial, (0,0))
-
+        TELA.blit(IMAGEMTELAINICIAL, (0,0))
 
 
         for event in pygame.event.get():
@@ -58,60 +72,127 @@ while True:
                 exit()
 
             elif event.type == KEYDOWN:
-
+                
+                # Verifica se o jogador quer iniciar o jogo e muda a maquina de estados
                 if event.key == K_SPACE:
                     estadoAtual = "Comecar jogo"
+
 
     elif estadoAtual == "Comecar jogo": 
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                exit()  
-
         relogio.tick(60)
-        tela.blit(imagemFundo, (0,0))
+        TELA.blit(IMAGEMFUNDO, (0,0))
+        PLAYER.mostrarPontos(TELA)
         
-
-        player.mostrarPontos(tela)
-
-
-        if player.jogadorPerdeu():
+        # Verifica se o jogador perdeu o jogo
+        if PLAYER.jogadorPerdeu():
             estadoAtual = "Tela final"
 
+        # MOSTRA OS OBJETOS NA TELA
+
+        NAVE.mostrarNave(TELA)
+        PLAYER.vidasJogador(TELA)
+        ASTEROIDE_1.aparecerAsteroide(TELA)
+        ASTEROIDE_2.aparecerAsteroide(TELA)
+        ASTEROIDE_3.aparecerAsteroide(TELA)
+        ASTEROIDE_4.aparecerAsteroide(TELA)
+        ASTEROIDE_5.aparecerAsteroide(TELA)
         
-    
-        # Funcionamento das teclas para mover a nave
-        if pygame.key.get_pressed()[K_LEFT]:
-            nave.andarEsquerda()    
-        
-        elif pygame.key.get_pressed()[K_RIGHT]:
-            nave.andarDireita()
 
+        # Mostra a nave inimiga e seus disparos
+        if NAVEINIMIGA.tempoSurgimento():
+            NAVEINIMIGA.aparecerNaveInimiga(TELA)
+ 
+            # Mostra os disparos da nave inimiga na tela
+            if DISPAROINIMIGO.contadorDisparo(NAVEINIMIGA):
+                DISPAROINIMIGO.disparar(TELA)  
+               
+                DISPAROINIMIGO.ajusteTrajetoria(NAVEINIMIGA)
+                DISPAROINIMIGO.trajetoria()
 
-        nave.mostrarNave(tela)
-        player.vidasJogador(tela)
+        DISPAROINIMIGO.ajusteDisparo(NAVEINIMIGA)
 
-    
-        
-        if buffs.contador():
+        # Mostra os buffs na tela
+        if BUFFS.contador():
 
-            if buffs.verificaEscolhaBuff():
+            if BUFFS.verificaEscolhaBuff():
                 pass
         
             else:
-                elemento = buffs.escolhaBuff()
+                elemento = BUFFS.escolhaBuff()
+
+            BUFFS.aparecerBuff(TELA, elemento)
+
+            PLAYER.colisaoBuff(NAVE, BUFFS, elemento, PLAYER, TELA)
+        
+
+        # MOVIMENTAÇÃO DA NAVE DO JOGADOR
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit() 
+
+            elif event.type == KEYDOWN:
+                # Verifica se o botão de disparo do jogador foi pressionado
+                if event.key == K_SPACE:
+                    DISPAROPLAYER.verificaTeclaApertada()
+
+        # Funcionamento das teclas para mover a nave
+        if pygame.key.get_pressed()[K_LEFT]:
+            NAVE.andarEsquerda()    
+            NAVEINIMIGA.moverNaveInimigaEsquerda()
+        
+        elif pygame.key.get_pressed()[K_RIGHT]:
+            NAVE.andarDireita()
+            NAVEINIMIGA.moverNaveInimigaDireita()
 
 
-            buffs.aparecerBuff(tela, elemento)
+        # SISTEMAS DE COLISÃO DE OBJETOS
 
-            player.colisaoBuff(nave, buffs, elemento, player, tela)
+        PLAYER.colisaoNaveJogador(NAVE, DISPAROINIMIGO)
+        PLAYER.colisaoNaveJogador(NAVE, ASTEROIDE_1)
+        PLAYER.colisaoNaveJogador(NAVE, ASTEROIDE_2)
+        PLAYER.colisaoNaveJogador(NAVE, ASTEROIDE_3)
+        PLAYER.colisaoNaveJogador(NAVE, ASTEROIDE_4)
+        PLAYER.colisaoNaveJogador(NAVE, ASTEROIDE_5)
+        
+        
+        if DISPAROPLAYER.apertouTecla():
+            PLAYER.colisaoDisparoJogador(DISPAROPLAYER, NAVEINIMIGA)
+            PLAYER.colisaoDisparoJogador(DISPAROPLAYER, ASTEROIDE_1)
+            PLAYER.colisaoDisparoJogador(DISPAROPLAYER, ASTEROIDE_2)
+            PLAYER.colisaoDisparoJogador(DISPAROPLAYER, ASTEROIDE_3)
+            PLAYER.colisaoDisparoJogador(DISPAROPLAYER, ASTEROIDE_4)
+            PLAYER.colisaoDisparoJogador(DISPAROPLAYER, ASTEROIDE_5)
+            
+     
+    
+       # SISTEMA DE EXECUÇÃO DE PODERES DOS BUFFS
 
-        if player.buffEstrela():
-            player.mostrarBuffEstrela(tela)
+
+        if PLAYER.buffEstrela():
+            PLAYER.mostrarBuffEstrela(TELA)
+
+
+        if PLAYER.buffArmamento():
+            DISPAROPLAYER.dispararComBuff(TELA)
+            DISPAROPLAYER.ajusteDisparo()
+            DISPAROPLAYER.ajusteTrajetoriaComBuff(NAVE)
+            DISPAROPLAYER.trajetoria()
+
+        else:           
+            DISPAROPLAYER.disparar(TELA)
+            DISPAROPLAYER.ajusteDisparo()
+            DISPAROPLAYER.ajusteTrajetoria(NAVE)
+            DISPAROPLAYER.trajetoria()
+
+
 
     elif estadoAtual == "Tela final":
-        tela.blit(imagemTelaFinal, (0,0))
+
+        TELA.blit(IMAGEMTELAFINAL, (0,0))
+        PLAYER.mostrarPontuacaoFinal(TELA)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -119,12 +200,18 @@ while True:
                 exit()
 
             elif event.type == KEYDOWN:
-
+                # Verifica se o jogador quer jogar novamente
                 if event.key == K_SPACE:
                     estadoAtual = "Comecar jogo"
+                    PLAYER.recomecarJogo()
+                    NAVEINIMIGA.recomecarJogo()
 
-
-        
-
+                    ASTEROIDE_1.recomecarJogo()
+                    ASTEROIDE_2.recomecarJogo()
+                    ASTEROIDE_3.recomecarJogo()
+                    ASTEROIDE_4.recomecarJogo()
+                    ASTEROIDE_5.recomecarJogo()
+                    
+                    BUFFS.recomecarJogo()
 
     pygame.display.update()
