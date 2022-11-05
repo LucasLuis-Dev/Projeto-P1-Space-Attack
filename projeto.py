7877ce6c0f05e5be386f6707a8b8fa80abb3000d
Mateus <mso3@cin.ufpe.br>
2022-11-05 00:41:10 -0300

    Modificações na parte de função e classes

--- projeto.py
+++ projeto.py
@@ -20,18 +20,18 @@ class Laser:
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
 
-    def move(self, vel):
+    def mover(self, vel):
         self.y += vel
 
-    def off_screen(self, height):
+    def fora_da_tela(self, height):
         return not(self.y <= height and self.y >= 0)
 
-    def collision(self, obj):
-        return collide(self, obj)
+    def colisao(self, obj):
+        return colidir(self, obj)
 
 
 class Nave:
-    cooldown = 30
+    tempo = 30
 
     def __init__(self, x, y, vida=100):
         self.x = x
@@ -47,30 +47,31 @@ class Nave:
         for laser in self.lasers:
             laser.draw(window)
 
-    def move_lasers(self, vel, obj):
-        self.cooldown()
+    def mover_lasers(self, vel, obj):
+        self.tempo()
         for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen('''aqui irá colocar a altura da tela'''):
-                self.lasers.remove(laser)
-            elif laser.collision(obj):
+            laser.mover(vel)
+            if laser.fora_da_tela(ALTURA):
+                self.lasers.remover(laser)
+            elif laser.colisao(obj):
                 obj.vida -= 10
-                self.lasers.remove(laser)
+                self.lasers.remover(laser)
 
-    def cooldown(self):
-        if self.cool_down_counter >= self.cooldown:
+    def tempo(self):
+        if self.cool_down_counter >= self.tempo:
             self.cool_down_counter = 0
         elif self.cool_down_counter > 0:
             self.cool_down_counter += 1
 
-    def shoot(self):
+    def tiro(self):
         if self.cool_down_counter == 0:
             laser = Laser(self.x, self.y, self.laser_img)
             self.lasers.append(laser)
             self.cool_down_counter = 1
+           
 
-    def get_width(self):
-        return self.nave_inimiga_img.get_width()
-
-    def get_height(self):
-        return self.nave_inimiga_img.get_height()
+    def obter_largura(self):
+        return self.nave_inimiga_img.obter_largura()
+    
+    def obter_altura(self):
+        return self.nave_inimiga_img.obter_altura()
